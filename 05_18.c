eae5f98438203d74210ea9e58b68ce276571794b
snack655 <80940200+snack655@users.noreply.github.com>
2021-05-18 14:07:27 +0900

    2021-05-18 Prime and Standard
    Prime and Standard

---
+++ 05_18.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//십진수를 16진수와 2진수로 바꿔 출력하시오.
+int main_05_18_atoi_itoa() {
+	//int i;
+	//char arr[50];
+	//char str[256]; // 1차원 배열
+	//printf("Enter a number: ");
+	//scanf("%d", &i);
+	//_itoa(i, arr, 10);
+	//printf("decimal : %s\n", arr);
+	//_itoa(i, arr, 16);
+	//printf("hexadecimal : %s\n", arr);
+	//_itoa(i, arr, 2);
+	//printf("binary : %s\n", arr);
+	// char* itoa(int value, char* str, int radix);
+
+	//fgets(buffer, 256, stdin);
+	//gets(str, 256); 
+	//i = atoi(str); // 문자열을 숫자로 변환
+	////atoi = char to int = 문자열을 정수 타입으로
+	////atof = char to double = 문자열을 실수 타입으로
+	////atol = char to long int = 문자열을 long 정수 타입으로
+	//printf("입력된 정수값 : %d, 2배의 값: %d. \n", i, i * 2);
+	return 0;
+}
---
+++ 05_18_Prime.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int isPrime(int n) { //소수(prime) 여부 판별 함수
+//	for (int i = 2; i*i <= n; i++)
+//		if (!(n % i)) // if(n%i == 0)
+//			return 0;
+//	return 1;
+//}
+
+int main_prime() {
+	/*printf("숫자 : ");
+	int a = 1, cnt = 0;
+	scanf("%d", &a);
+	if (a <= 0 || a > 1000)
+		break;
+	for (int i = 2; i < a; i++) {
+		if (isPrime(i)) {
+			printf("%4d ", i);
+			if (cnt % 10 == 0)
+				printf("\n");
+			cnt++;
+		}
+	}*/
+	return 0;
+}
