ef91bc171a9a527b4797c01624fe1ae49ef231c2
snack655 <80940200+snack655@users.noreply.github.com>
2021-07-10 22:32:36 +0900

    2021-07-10 arduino NSK code
    hackathon
    arduino
    NSK code

---
+++ door.ino
@@ -0,0 +1,133 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+int echoPin = 12;
+int trigPin = 13;
+
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(200); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(200);
+
+  
+
+  display.invertDisplay(true);
+  delay(100);
+  display.invertDisplay(false);
+  delay(100);
+
+}
+
+
+
+void loop() {
+ 
+  float duration, distance;
+  // 초음파를 보낸다. 다 보내면 echo가 HIGH 상태로 대기하게 된다.
+  digitalWrite(trigPin, HIGH);
+  delay(10);
+  digitalWrite(trigPin, LOW);
+  
+  // echoPin 이 HIGH를 유지한 시간을 저장 한다.
+  duration = pulseIn(echoPin, HIGH); 
+  // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
+  distance = ((float)(340 * duration) / 10000) / 2;  
+  
+  Serial.print(distance);
+  Serial.println("cm");
+  // 수정한 값을 출력
+  delay(500);
+
+  if(distance <= 10) {
+    testdrawstyles();
+
+  }
+
+  if(distance >= 20) {
+    delay(200);
+    ENDdrawstyles();
+  }
+  
+
+}
+
+void testdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("warning!!!"));
+
+  display.setTextColor(SSD1306_WHITE); // Draw 'inverse' text
+  display.setTextSize(3);
+  display.println("NSK");
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.println("OPEN DOOR");
+
+  display.display();
+  delay(1000);
+}
+
+void ENDdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("NO"));
+
+  display.display();
+  delay(1000);
+}
